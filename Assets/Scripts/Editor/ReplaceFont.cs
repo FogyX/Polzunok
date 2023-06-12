3e093878a2222af5c399cf93f0d527278bc53bff
FogyX <maksdima0515@gmail.com>
2023-06-12 16:06:15 +0300

    Initial commit

---
+++ Assets/Scripts/Editor/ReplaceFont.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Editor
+{
+    public class ReplaceFont : EditorWindow
+    {
+        private Font _oldFont;
+        private Font _newFont;
+        
+        [MenuItem("Tools/Replace Font")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<ReplaceFont>();
+            window.titleContent = new GUIContent("Replace font");
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            _oldFont = (Font)EditorGUILayout.ObjectField("Old font", _oldFont, typeof(Font), false);
+            _newFont = (Font)EditorGUILayout.ObjectField("New font", _newFont, typeof(Font), false);
+
+            if (GUILayout.Button("Change font"))
+            {
+                Text[] texts = FindObjectsOfType<Text>();
+
+                foreach (var text in texts)
+                {
+                    if (text.font == _oldFont)
+                        text.font = _newFont;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PopupRewardVideo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using YG;
+public class PopupRewardVideo : MonoBehaviour
+{
+    [SerializeField] private int _adID;
+
+    private void AddHintReward(int id)
+    {
+        if (id == _adID)
+        {
+            GameDefine.hintCount += 1;
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        YandexGame.RewardVideoEvent += AddHintReward;
+    }
+
+    private void OnDisable()
+    {
+        YandexGame.RewardVideoEvent -= AddHintReward;
+    }
+}
